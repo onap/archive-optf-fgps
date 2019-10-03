f58d70e5dbe6737d095c27515d92a0fb4404e7ac
Thugutla sailakshmi <tsaila10@in.ibm.com>
2019-10-03 15:10:48 +0530

    changed the port to  "nexus3.onap.org:10001"
    changed the port to  "nexus3.onap.org:10001"
    
    Issue-ID: OPTFRA-578
    Change-Id: Iba482a06fc950e14da448056842666aed8120a04
    Signed-off-by: Thugutla sailakshmi <tsaila10@in.ibm.com>

--- valetapi/build-dockers.sh
+++ valetapi/build-dockers.sh
@@ -10,7 +10,7 @@ set -e
 BUILD_ARGS="--no-cache"
 ORG="onap"
 PROJECT="fgps-api"
-DOCKER_REPOSITORY="nexus.onap.org:5100"
+DOCKER_REPOSITORY="nexus3.onap.org:10001"
 IMAGE_NAME="${DOCKER_REPOSITORY}/${ORG}/${PROJECT}"
 
 # Version properties
